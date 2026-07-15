a9955715af5755912079c8e3ad5b2e4955c3616b
Hendrick Herrera <hendrickgerman@hotmail.com>
2026-07-15 15:42:14 -0400

    feat: added best score and score saving

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,9 +1,39 @@
 extends Control
 
+var savePath: String = "user://userdata.save"
+
 #const MENU: PackedScene = preload("res://scenes/StartMenu/StartMenu.tscn")
 
 var presses: int = 0;
+var best_presses: int = 0;
 signal press_change;
+
+func _ready() -> void:
+	load_data()
+	print(presses)
+	emit_signal("press_change", presses, best_presses)
+
+
+func save_data():
+	var data = {
+		"current_presses": presses,
+		"best_presses": best_presses
+	}
+	var file = FileAccess.open(savePath, FileAccess.WRITE)
+	file.store_var(data)
+	file.close()
+
+func load_data():
+	if (FileAccess.file_exists(savePath)):
+		var file = FileAccess.open(savePath, FileAccess.READ)
+		var data = file.get_var()
+		file.close()
+		
+		if (typeof(data) == TYPE_DICTIONARY):
+			presses = data.get("current_presses", 0)
+			best_presses = data.get("best_presses", 0)
+		else:
+			save_data()
 
 func _process(delta: float) -> void:
 	if(Input.is_action_just_pressed("Press")):
@@ -11,10 +41,14 @@ func _process(delta: float) -> void:
 
 
 func _on_texture_button_button_down() -> void:
-	#get_tree().change_scene_to_fil	e("res://scenes/StartMenu/StartMenu.tscn");
 	if ((randi() % 3) == 1):
 		print("change")
+		presses = 0
 		get_tree().change_scene_to_file("res://scenes/StartMenu/StartMenu.tscn");
 
 	presses += 1;
-	emit_signal("press_change", presses)
+	if (presses > best_presses):
+		best_presses = presses;
+
+	emit_signal("press_change", presses, best_presses)
+	save_data()
--- scripts/stats.gd
+++ scripts/stats.gd
@@ -3,6 +3,7 @@ extends VBoxContainer
 @onready var v_box_container: VBoxContainer = $"."
 @onready var label: Label = $Label
 
-func _on_control_press_change(amount) -> void:
-	label.text = "Presses: " +  str(amount)
+func _on_control_press_change(amount, best_amount) -> void:
+	label.text = "Presses: " +  str(amount) + " | " + "Best Presses: " + str(best_amount)
+	# Presses: 0 | Best Presses: 0 
 	
